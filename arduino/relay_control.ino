d9afd3503eda7e06f8db246e670fbc28e99ea4b8
marcus <best.nose@gmail.com>
2016-08-26 18:40:33 +1200

    tidied up sketch and tested successfully

--- arduino/relay_control.ino
+++ arduino/relay_control.ino
@@ -5,21 +5,25 @@ const int ledPin =  13;      // the number of the LED pin
 volatile int inputPin = 2;         // variable for reading the pushbutton status
 
 void setup() {
+  //Serial.begin(9600);
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
-  pinMode(relayPin, INPUT);
-  // Attach an interrupt to the ISR vector
-  attachInterrupt(digitalPinToInterrupt(inputPin),pin_ISR , RISING);
+  pinMode(relayPin, OUTPUT);
+  // Attach an interrupt to the ISR vector (digital interrupt 0 maps to D2)
+  attachInterrupt(0,pin_ISR , RISING);
 }
 
 void loop() {
   //test to see if interrupt has been called and delay if it has
   if (digitalRead(relayPin) == HIGH){
+    //Serial.println("received high in loop, doing stuff");
+    digitalWrite(ledPin, LOW);
     delay(1000);
     digitalWrite(relayPin, LOW);
   }
   else {
+    //Serial.println("doign nothing");
     //do nothing
   }
 }
@@ -27,5 +31,6 @@ void loop() {
 
 void pin_ISR() {
   digitalWrite(relayPin, HIGH);
+  //Serial.println("Received interrupt");
 }
 
