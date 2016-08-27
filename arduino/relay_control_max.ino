a258d2d8e0ffae98cb4fa8ff541433811c95207e
marcus <best.nose@gmail.com>
2016-08-27 22:06:20 +1200

    updated max's script

--- arduino/relay_control_max.ino
+++ arduino/relay_control_max.ino
@@ -1,70 +1,87 @@
-const int relayPin = 5;     // the number of pin to drive relay
-const int ledPin =  13;      // the number of the LED pin
-const int inputPin = 2;         // variable for reading the pushbutton status
+const int inputpin = 2;
+const int relaypin = 5;
+const int ledpin = 13;
 
-// variables will change:
-//volatile int inputPin = 2;         // variable for reading the pushbutton status
-//volatile int start_time;
-int start_time;
-const int wait_time_min = 300;    // time [ms] for
-const int wait_time_max = 400;    // time [ms] for
-const int cut_off_time = 750;  // time [ms] for input pin to be HIGH before deciding to trigger relay 
-const int trigger_duration = 100;  // time [ms] that relay is pulled HIGH for.
-int started;
+volatile int STATE;
+volatile unsigned long time_since_trigger;
 
-void setup() {
-  //Serial.begin(9600);
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(relayPin, OUTPUT);
-  // Attach an interrupt to the ISR vector (digital interrupt 0 maps to D2)
-  //attachInterrupt(0,pin_ISR , RISING);
-  pinMode(inputPin, INPUT);
-  start_time = millis();
-  started = 0;
+const int WAITING = 1;
+const int PRELIMINARY_TRIGGER = 2;
+const int TRIGGERED = 3;
+const int TOO_LONG_TRIGGER = 4;
+
+const int min_triggertime = 300;
+const int max_triggertime = 1000;
+const int relay_high_time = 100;
+const int cool_down_time = 1;
+
+
+void setup(){
+  //start serial connection
+  Serial.begin(115200);
+  //configure pin2 as an input and enable the internal pull-up resistor
+  pinMode(inputpin, INPUT);
+  pinMode(relaypin, OUTPUT); 
+  pinMode(ledpin, OUTPUT);
+  digitalWrite(relaypin, LOW);
+  digitalWrite(ledpin, LOW);
+  STATE = WAITING;
 }
 
-void loop() {
-  //test to see if interrupt has been called and delay if it has
-  if ((millis() - start_time)> cut_off_time){
-    // do nothing
-    if started {
-      trigger();
-      started = 0;
+void loop(){
+  if (STATE == WAITING) {waiting();}
+  else if (STATE == PRELIMINARY_TRIGGER) {pre_trigger();}
+  else if (STATE == TRIGGERED) {triggered();}
+  else if (STATE == TOO_LONG_TRIGGER) {too_long();}
+}
+
+
+void waiting(){
+  if (digitalRead(inputpin) == HIGH) {
+    STATE = PRELIMINARY_TRIGGER;
+    Serial.println("going to PRELIMINARY_TRIGGER state");
+    time_since_trigger = millis();
+    Serial.println(time_since_trigger);
+  }
+}
+
+void pre_trigger(){
+  int dt = millis() - time_since_trigger;
+  if ( digitalRead(inputpin) == HIGH) {  
+    if (dt > max_triggertime) {
+      STATE = TOO_LONG_TRIGGER;
+      Serial.println(time_since_trigger);
+      Serial.println("going to TOO_LONG_TRIGGER state");
+    }
+  } else {
+    // input pin is LOW
+    if  (dt >  min_triggertime){
+      STATE = TRIGGERED;
+      Serial.println("going to TRIGGERED state");
     } else {
-      start_time = millis();
+      STATE = WAITING;
+      Serial.println("going to WAITING state");
     }
-  }
-  
-  if ((millis() - start_time) > wait_time_min) && ((millis() - start_time) < wait_time_max) {  
-    if (digitalRead(inputPin) == HIGH){
-      started = 1;
-    }
-  
-//      digitalWrite(ledPin, HIGH);
-//      digitalWrite(relayPin, HIGH);
-//      delay(trigger_duration);
-//      digitalWrite(ledPin, LOW);
-//      digitalWrite(relayPin, LOW); 
-//      start_time = millis();
-     //   started = 0;
-     // }
-    }
-    //Serial.println("received high in loop, doing stuff");
-  }
-  else {
-    
-    //start_time = millis();
-    //started = 0;
-    //Serial.println("doign nothing");
-    //do nothing
   }
 }
 
+void triggered(){
+  digitalWrite(relaypin, HIGH);
+  digitalWrite(ledpin, HIGH);
+  delay(relay_high_time);
+  digitalWrite(relaypin, LOW);
+  digitalWrite(ledpin, LOW);
+  delay(cool_down_time);
+  STATE = WAITING;
+  Serial.println("going to WAITING state");
+}
 
-//void pin_ISR() {
-//  digitalWrite(relayPin, HIGH);
-//  //Serial.println("Received interrupt");
-//}
-
+void too_long(){
+  if (digitalRead(inputpin) == LOW) {
+    delay(cool_down_time);
+    STATE = WAITING;
+    time_since_trigger = millis();
+    Serial.println(time_since_trigger);
+    Serial.println("going to WAITING state");
+  }
+}
