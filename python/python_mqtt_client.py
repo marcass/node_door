8d86e68498c4090f090e992609d830dcbec047b2
Marcus <best.nose@gmail.com>
2018-06-14 11:06:07 +1200

    managing sleeping better

--- python/python_mqtt_client.py
+++ python/python_mqtt_client.py
@@ -2,9 +2,15 @@
 import time
 import paho.mqtt.client as mqtt
 import os
+import threading
 
 broker = "nuc"
 sound = '/root/bin/sounds/Cow-moo-sound.mp3'
+quiet = False
+
+def reset_timer():
+    global quiet
+    quiet = False
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -15,13 +21,21 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
+    global quiet
     print(msg.topic+' '+str(msg.payload))
     #allowed_passthrough_msg = ['Opened']
     if 'Opened' in msg.payload:
-        os.system('mpg123 ' + sound)
-        time.sleep(60)
-        print 'Played ' + sound + ' and sleeping for 60s'
-        #stops door chime going constantly
+        if quiet:
+            print 'sleeping'
+            return
+        else:
+            os.system('mpg123 ' + sound)
+            print 'Played ' + sound + ' and sleeping for 20s'
+            quiet = True
+            t = threading.Timer(20.0, reset_timer)
+            # time.sleep(60)
+            #stops door chime going constantly
+            return
 
 if __name__ == "__main__":
     client = mqtt.Client()
